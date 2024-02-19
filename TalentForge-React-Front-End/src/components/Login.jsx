ad5f7189db3951eba3ba6fbe11a63115443d19db
Kelly <kelly.degannes@gmail.com>
2024-02-19 15:29:01 +1100

    Added state, logic and redirect to home on login page

--- TalentForge-React-Front-End/src/components/Login.jsx
+++ TalentForge-React-Front-End/src/components/Login.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
-  const logo_filepath = "./assets/talent_forge_logo.jpg";
 
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const nav = useNavigate();
+
+    function checkCredentials(e) {
+        if (username && password) {
+            e.preventDefault();
+            // Find username in database
+            console.log("Username: ", username);
+
+            // Check matching password in database
+            console.log("Password: ", password);
+
+            // If username and password match, redirect to homepage
+            nav(`/home`)
+
+            // else:
+            // If username and password do not match, alert user
+        } else {
+            e.preventDefault();
+            alert("Please enter a username and password");
+        }
+
+    }
 
     return (
         <>
             <div className="login">
-                <img src={logo_filepath} alt="talent forge logo" />
-                <form>
-                    <label>Username</label>
-                    <input type="text" placeholder="Username" />
-                    <label>Password</label>
-                    <input type="password" placeholder="Password" />
+                <img src="src/assets/talent_forge_logo.jpg" alt="talent forge logo" width="200" height="200"/>
+                <form onSubmit={checkCredentials}>
+                    <label>Username: </label>
+                    <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+                    <label>Password: </label>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
                     <button type="submit">Login</button>
                 </form>
 
