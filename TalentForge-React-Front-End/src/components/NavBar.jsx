e5fdcb6e1a71adc434115bc0a9dad23ae998d42f
Matt-bryant21 <14771@coderacademy.edu.au>
2024-02-19 23:42:26 +1100

    put alt logo back in

--- TalentForge-React-Front-End/src/components/NavBar.jsx
+++ TalentForge-React-Front-End/src/components/NavBar.jsx
@@ -36,13 +36,13 @@ export default function navBar() {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+                <div className="flex flex-shrink-0 items-center justify-center">
                   {/* logo image */}
-                  {/* <img
-                    className="h-20 w-auto"
-                    src="./src/assets/text-only-logo.png"
+                  <img
+                    className="h-12 w-auto"
+                    src="./src/assets/newtransplogo.png"
                     alt="Talent Forge Logo"
-                  /> */}
+                  />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4 justify-center">
