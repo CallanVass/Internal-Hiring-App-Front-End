8b1906d811704d088064ae425a2a30ca1ae45e98
Kelly <kelly.degannes@gmail.com>
2024-02-20 20:55:03 +1100

    Added responsive design to homepage

--- TalentForge-React-Front-End/src/components/HomePage.jsx
+++ TalentForge-React-Front-End/src/components/HomePage.jsx
@@ -8,9 +8,9 @@ const HomePage = () => {
       </div>
 
       <div className="flex justify-center">
-      <h1 className='text-white text-7xl absolute inset-x-40 inset-y-44'>Welcome &#39;user.name&#39; to TalentForge</h1>
+      <h1 className='text-white text-5xl lg:text-7xl absolute inset-y-28 md:inset-y-44 max-w-full md:max-w-lg lg:max-w-3xl sm:px-10'>Welcome &#39;user.name&#39; to TalentForge</h1>
 
-      <h2 className='text-white text-4xl absolute inset-y-72 max-w-3xl'>This is your home of....explore, interact, opportunities, develop...mission statement/blurb/ intro to let users know about the site and all of the benefits they can expect from it...<br/><br/>Select one of the links below to get started!</h2>
+      <h2 className='text-white text-3xl lg:text-4xl absolute inset-y-72 md:inset-y-96 max-w-full md:max-w-lg lg:max-w-3xl sm:px-10'>This is your home of....explore, interact, opportunities, develop...mission statement/blurb/ intro to let users know about the site and all of the benefits they can expect from it...<br/><br/>Select one of the links below to get started!</h2>
       <button type="submit" className=" bg-grey hover:bg-dark-blue text-dark-blue font-semibold hover:text-white m-2 py-1 px-5 border border-blue-500 hover:border-transparent rounded">Login</button>
 
       </div>
