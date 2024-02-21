437effe8ed77aa41e81e9013881d924cf699a16f
Kelly <kelly.degannes@gmail.com>
2024-02-21 16:33:49 +1100

    Added View Listing JSX file

---
+++ TalentForge-React-Front-End/src/components/ViewListing.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ViewListing = ({ listing }) => {
+    // const listing = { id, title, description, location, salary, datePosted, dateClosing, roleType, roleDuration, status, applicants }
+  return (
+    <div>ViewListing</div>
+  )
+}
+
+export default ViewListing
