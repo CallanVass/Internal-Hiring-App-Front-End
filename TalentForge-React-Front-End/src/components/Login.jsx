9e66d1bc7fcf194ec77e19cedfbbedb1de25705d
Kelly <kelly.degannes@gmail.com>
2024-02-25 21:44:44 +1100

    Added Matt's conflicting changes

--- TalentForge-React-Front-End/src/components/Login.jsx
+++ TalentForge-React-Front-End/src/components/Login.jsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-
 import '../assets/css/Login.css';
-import { Container } from "postcss";
 
 
 
@@ -13,13 +11,10 @@ const Login = () => {
     const nav = useNavigate();
 
     // Get username and password from form
-    useEffect(() => {
 
-    })
 
     async function checkCredentials(e) {
-        console.log("Username: ", username)
-        console.log("Password: ", password)
+
         const loginCredentials = {
             email: username,
             password: password
@@ -35,48 +30,53 @@ const Login = () => {
                 },
                 body: JSON.stringify(loginCredentials)
             });
-            const response = await res.json()
-            console.log(response); // This will be the token
-            // Find username in database
-            // console.log("Username: ", username);
+            const response = await res.json() // This will be the token
+            // If token is present in the response, redirect to homepage
+            if (response.token) {
+                nav(`/home`)
+            } else {
+                // navigate to error page...
+                console.log(response)
+            }
 
-            // Check matching password in database
-            // console.log("Password: ", password);
-
-            // If username and password match, redirect to homepage
-            nav(`/home`)
-
-            // else:
-            // If username and password do not match, alert user
         } else {
             e.preventDefault();
-            alert("Please enter a username and password");
+            alert("Please enter an email and password");
         }
 
     }
 
     return (
         <>
-            <div className="flex-col text-center">
-
-                <img className="m-2 inline" src="src/assets/logos/talent_forge_logo.jpg" alt="talent forge logo" width="200" height="200"/>
-                <form onSubmit={checkCredentials}>
-                    <span className="block m-3">
-                    <label className="">Email: </label>
-                    <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
-                    </span>
-                    <span className="block m-5">
-                    <label className="m-1">Password: </label>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-                    </span>
-                    <button type="submit" title="login-btn" className="bg-grey hover:bg-dark-blue text-dark-blue font-semibold hover:text-white m-2 py-1 px-5 border border-blue-500 hover:border-transparent rounded">Login</button>
-                </form>
-                <p className="pt-6">Not registered? Contact your HR </p>
-                <p>department to be set up</p>
-
-            </div>
+          <div className="flex-col text-center">
+            <img
+              className="m-2 inline"
+              src="src/assets/logos/talent_forge_logo.jpg"
+              alt="talent forge logo"
+              width="200"
+              height="200"
+            />
+            <form onSubmit={checkCredentials}>
+              <span className="block m-3">
+                <label htmlFor="username" className="">Username: </label>
+                <input id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+              </span>
+              <span className="block m-5">
+                <label htmlFor="password" className="m-1">Password: </label>
+                <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </span>
+              <button
+                type="submit"
+                title="login-btn"
+                className="bg-grey hover:bg-dark-blue text-dark-blue font-semibold hover:text-white m-2 py-1 px-5 border border-blue-500 hover:border-transparent rounded"
+              >
+                Login
+              </button>
+            </form>
+            <p className="pt-6">Not registered? Contact your HR </p>
+            <p>department to be set up</p>
+          </div>
         </>
-    )
-}
-
+      );
+    };
 export default Login;
