385f1c20d9bed86a7248cb68f086205ff2f56173
Matt-bryant21 <14771@coderacademy.edu.au>
2024-02-28 10:39:47 +1100

    changed homepage top bottom margin

--- TalentForge-React-Front-End/src/components/HomePage.jsx
+++ TalentForge-React-Front-End/src/components/HomePage.jsx
@@ -11,8 +11,8 @@ const HomePage = () => {
         <div className="bg-hero bg-cover bg-bottom min-h-screen">
 
         {/* Text content */}
-        <div className="flex justify-center p-2 ">
-        <div className="flex flex-col justify-center my-4">
+        <div className="flex justify-center p-2">
+        <div className="flex flex-col justify-center my-20">
           <h1 className='text-white text-5xl md:text-6xl lg:text-7xl inset-y-28 h-fit md:inset-y-44 max-w-full md:max-w-lg lg:max-w-3xl px-10 mb-5'>Welcome &#39;user.name&#39; to TalentForge</h1>
           <h2 className='text-white text-3xl lg:text-4xl inset-y-72 md:inset-y-96 h-fit max-w-full md:max-w-lg lg:max-w-3xl px-10 mb-5'>This is your home of....explore, interact, opportunities, develop...mission statement/blurb/ intro to let users know about the site and all of the benefits they can expect from it...<br/><br/>Select one of the links below to get started!</h2>
         </div>
