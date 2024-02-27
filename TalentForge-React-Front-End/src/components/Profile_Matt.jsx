55206b79ea14d87fef7606ddda4e8a4275b655c1
Matt-bryant21 <14771@coderacademy.edu.au>
2024-02-27 23:10:13 +1100

    profile changes

--- TalentForge-React-Front-End/src/components/Profile_Matt.jsx
+++ TalentForge-React-Front-End/src/components/Profile_Matt.jsx
@@ -1,88 +1,83 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext } from "react";
 
-
-
-
-const Profile_Matt =  () => {
-
+const Profile_Matt = () => {
   const users = [
     {
-      "_id": "65dd2ca89d0a768b6605a4b3",
-      "firstName": "Adam",
-      "lastName": "Andrews",
-      "email": "adam@email.com",
-      "password": "$2b$08$SHqnZN3OS2/FKd8NYtP6S.TdBJwIVyDZfMfWfxeYHngK0m/Z8zziC",
-      "role": "Recruitment Manager",
-      "department": "Human Resources",
-      "image": "./misc/male_user_image.jpeg",
-      "admin": true,
-      "applications": [],
-      "__v": 0
+      _id: "65dd2ca89d0a768b6605a4b3",
+      firstName: "Adam",
+      lastName: "Andrews",
+      email: "adam@email.com",
+      password: "$2b$08$SHqnZN3OS2/FKd8NYtP6S.TdBJwIVyDZfMfWfxeYHngK0m/Z8zziC",
+      role: "Recruitment Manager",
+      department: "Human Resources",
+      image: "./misc/male_user_image.jpeg",
+      admin: true,
+      applications: [],
+      __v: 0,
     },
     {
-      "_id": "65dd2ca89d0a768b6605a4b4",
-      "firstName": "Betty",
-      "lastName": "Bailey",
-      "email": "betty@email.com",
-      "password": "$2b$08$YLAX7rqWk5IgW2fQFkL7vuGJDumTDMuSNRuFaG2gGWEfC2XfGpN0S",
-      "role": "HR Specialist",
-      "department": "Human Resources",
-      "image": "./misc/female_user_image.jpeg",
-      "admin": false,
-      "applications": [],
-      "__v": 0
+      _id: "65dd2ca89d0a768b6605a4b4",
+      firstName: "Betty",
+      lastName: "Bailey",
+      email: "betty@email.com",
+      password: "$2b$08$YLAX7rqWk5IgW2fQFkL7vuGJDumTDMuSNRuFaG2gGWEfC2XfGpN0S",
+      role: "HR Specialist",
+      department: "Human Resources",
+      image: "./misc/female_user_image.jpeg",
+      admin: false,
+      applications: [],
+      __v: 0,
     },
     {
-      "_id": "65dd2ca89d0a768b6605a4b5",
-      "firstName": "Charlie",
-      "lastName": "Castleton",
-      "email": "charlie@email.com",
-      "password": "$2b$08$W1jXuLHJkS01R1xKhwXneOZsNR7Wq3ufs05CogClBC4VP.44xlI5u",
-      "role": "Software Engineer",
-      "department": "IT",
-      "image": "./misc/male_user_image.jpeg",
-      "admin": false,
-      "applications": [],
-      "__v": 0
+      _id: "65dd2ca89d0a768b6605a4b5",
+      firstName: "Charlie",
+      lastName: "Castleton",
+      email: "charlie@email.com",
+      password: "$2b$08$W1jXuLHJkS01R1xKhwXneOZsNR7Wq3ufs05CogClBC4VP.44xlI5u",
+      role: "Software Engineer",
+      department: "IT",
+      image: "./misc/male_user_image.jpeg",
+      admin: false,
+      applications: [],
+      __v: 0,
     },
     {
-      "_id": "65dd2ca89d0a768b6605a4b6",
-      "firstName": "David",
-      "lastName": "Denley",
-      "email": "david@email.com",
-      "password": "$2b$08$4iQHdxf3TWrjZZdsDR1ubudqUVqaSwzY6FhSaS5w/jw1Mbqc0xgPe",
-      "role": "Product Manager",
-      "department": "Production",
-      "image": "./misc/male_user_image.jpeg",
-      "admin": false,
-      "applications": [],
-      "__v": 0
+      _id: "65dd2ca89d0a768b6605a4b6",
+      firstName: "David",
+      lastName: "Denley",
+      email: "david@email.com",
+      password: "$2b$08$4iQHdxf3TWrjZZdsDR1ubudqUVqaSwzY6FhSaS5w/jw1Mbqc0xgPe",
+      role: "Product Manager",
+      department: "Production",
+      image: "./misc/male_user_image.jpeg",
+      admin: false,
+      applications: [],
+      __v: 0,
     },
     {
-      "_id": "65dd2ca89d0a768b6605a4b7",
-      "firstName": "Adam",
-      "lastName": "Hunter",
-      "email": "adamh@email.com",
-      "password": "$2b$08$.BkQP9c3KXQ4oJCPHfYFw.NDwDvRREQxRHPKheE5y4Id5IhE.aRTS",
-      "role": "Developer",
-      "department": "IT",
-      "image": "./misc/male_user_image.jpeg",
-      "admin": false,
-      "aboutMe": {
-        "text": "I am a developer",
-        "careerDevelopment": "I want to be a senior developer",
-        "tags": [
+      _id: "65dd2ca89d0a768b6605a4b7",
+      firstName: "Adam",
+      lastName: "Hunter",
+      email: "adamh@email.com",
+      password: "$2b$08$.BkQP9c3KXQ4oJCPHfYFw.NDwDvRREQxRHPKheE5y4Id5IhE.aRTS",
+      role: "Developer",
+      department: "IT",
+      image: "./misc/male_user_image.jpeg",
+      admin: false,
+      aboutMe: {
+        text: "I am a developer",
+        careerDevelopment: "I want to be a senior developer",
+        tags: [
           "Looking for a new job!",
           "Happy where I am!",
           "Might be willing to move!",
-          "Unsure how I feel about it!"
-        ]
+          "Unsure how I feel about it!",
+        ],
       },
-      "applications": [],
-      "__v": 0
-    }
-  ]
-
+      applications: [],
+      __v: 0,
+    },
+  ];
 
   // Checkboxes Dummy Data
   const statuses = [
@@ -152,7 +147,6 @@ const Profile_Matt =  () => {
     department: users[4].department,
     // aboutMe: users[4].aboutMe.text,
     // careerDevelopment: users[4].aboutMe.careerDevelopment,
-
   });
 
   const toggleEditMode = () => setIsEditMode(!isEditMode);
@@ -202,12 +196,14 @@ const Profile_Matt =  () => {
                   />
                 </div>
               ) : (
-                <h2 className="text-3xl font-bold mb-2">{`${editableProfile.firstName} ${" "} ${editableProfile.lastName}`}</h2>
+                <h2 className="text-3xl font-bold mb-2">{`${editableProfile.firstName} ${" "} ${
+                  editableProfile.lastName
+                }`}</h2>
               )}
 
               {/* Edit Role */}
               {isEditMode ? (
-                <div className="flex items-center mb-4">
+                <div className="flex items-center mb-4 w-full">
                   <div className="mr-2">Role:</div>
                   <input
                     type="text"
@@ -224,7 +220,7 @@ const Profile_Matt =  () => {
 
               {/* Edit Department */}
               {isEditMode ? (
-                <div className="flex items-center mb-4">
+                <div className="flex items-center mb-4 w-full">
                   <div className="mr-2">Department: </div>
                   <input
                     type="text"
@@ -244,7 +240,7 @@ const Profile_Matt =  () => {
           {/* Skills list */}
           {isEditMode ? (
             // If isEditMode is true, then display edit options
-            <div className="flex justify-center items-center max-w-lg mx-auto px-5 flex-col">
+            <div className="flex justify-center items-center flex-col">
               {" "}
               {/* Changed class to flex-col */}
               <div className="flex flex-wrap gap-2 items-center space-x-0.5 mt-10 px-5 flex-col">
@@ -264,8 +260,10 @@ const Profile_Matt =  () => {
                   </div>
                 ))}
               </div>
-              {/* Add new skill */}
-              <div className="flex justify-center items-center max-w-lg mx-auto mt-10 px-5 flex-col"> {/* Added flex class */}
+              {/* Add new skill div */}
+              <div className="flex justify-center items-center my-3 flex-col">
+                {" "}
+                {/* Increased max-w-lg to max-w-xl */}
                 <input
                   maxLength="15"
                   placeholder="(15 character max)"
@@ -277,8 +275,7 @@ const Profile_Matt =  () => {
                 <button
                   onClick={addSkill}
                   data-testid="add-skill-button"
-                  className="bg-dark-green text-white shadow shadow-gray-300 px-4 py-2 rounded-md mt-2 self-center"
-
+                  className="bg-dark-green text-white shadow shadow-gray-300 px-4 py-2 rounded-md mt-2 self-center hover:bg-dark-blue"
                 >
                   Add Skill
                 </button>
@@ -298,7 +295,7 @@ const Profile_Matt =  () => {
 
           {/* Profile Description (About Me) */}
           {isEditMode ? (
-            <div className="flex flex-col max-w-lg mx-auto mt-10 px-5 lg:mb-10">
+            <div className="flex flex-col items max-w-lg mx-auto mt-10 px-5 lg:mb-10">
               <label htmlFor="aboutMe" className="text-center mb-3 text-xl">
                 About Me:
               </label>
