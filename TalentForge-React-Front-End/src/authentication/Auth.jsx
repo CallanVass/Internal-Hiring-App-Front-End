5bb28da9809ec357c29206cf0774b3d8cf14586a
Kelly <kelly.degannes@gmail.com>
2024-02-29 23:17:46 +1100

    CurrentUserContext now stored and available

--- TalentForge-React-Front-End/src/authentication/Auth.jsx
+++ TalentForge-React-Front-End/src/authentication/Auth.jsx
@@ -1,27 +1,27 @@
 // This module provides a function to obtain the user and token and return matching status to the caller
 // If the user is an admin, the function returns true
 // If the profile's user id is the same as the token, the function returns true otherwise it returns false
-import CurrentUser from './CurrentUser'
+
+// Ensure the profile is passed in as the ID ONLY, and the current user is passed in as the ENTIRE OBJECT
 
 
-export default function Auth(profileUserId) {
-  const token = sessionStorage.getItem('token')
-  const currentUser = CurrentUser() // Returns user object or null
+export default function Auth(profileUserId, currentUser) {
 
-  // const tokenDecode = decoder(token)
   console.log(currentUser)
-  console.log(token)
+  console.log(currentUser._id)
   console.log(profileUserId)
 
   let match
 
-    if (currentUser?.isAdmin === true) {
+    if (currentUser?.admin) {
       match = true
     } else if (profileUserId === currentUser._id) {
       match = true
     } else {
       match = false
     }
+    console.log(currentUser._id)
+    console.log(profileUserId)
 
   return match
 }
